25b6a24b53429428ed0a9315e4687e00671a71cd
yatankek <ninja_mos@mail.ru>
2025-10-18 01:29:09 +0300

    небольшие правки

--- lib/features/orders/logic/order_service.dart
+++ lib/features/orders/logic/order_service.dart
@@ -1,31 +1,30 @@
 import 'package:prac5/features/orders/data/models/order.dart';
 import 'package:prac5/features/orders/data/repository/order_repository.dart';
+import 'package:prac5/features/orders/data/repository/order_history_repository.dart';
 
 class OrderService {
-  final OrderRepository _repository = OrderRepository();
+  final OrderRepository _orderRepository = OrderRepository();
+  final OrderHistoryRepository _orderHistoryRepository = OrderHistoryRepository();
 
-  // История оформленных заказов
-  final List<List<Order>> _orderHistory = [];
+  List<Order> get orders => _orderRepository.getAll();
 
-  List<Order> get orders => _repository.getAll();
-
-  List<List<Order>> get orderHistory => List.unmodifiable(_orderHistory);
+  List<List<Order>> get orderHistory => _orderHistoryRepository.getAll();
 
   void addOrder(String name, int quantity, double price) {
     if (name.isEmpty || quantity <= 0 || price <= 0) return;
-    _repository.addOrder(Order(name: name, quantity: quantity, price: price));
+    _orderRepository.addOrder(Order(name: name, quantity: quantity, price: price));
   }
 
-  void removeOrder(Order order) => _repository.removeOrder(order);
+  void removeOrder(Order order) => _orderRepository.removeOrder(order);
 
-  double getTotalSum() => _repository.getTotalSum();
+  double getTotalSum() => _orderRepository.getTotalSum();
 
   // Оформление заказа: перенос в историю и очистка текущих заказов
   void payOrder() {
-    final currentOrders = _repository.getAll();
+    final currentOrders = _orderRepository.getAll();
     if (currentOrders.isNotEmpty) {
-      _orderHistory.add(List.from(currentOrders));
-      _repository.clearOrders();
+      _orderHistoryRepository.addOrdersToHistory(currentOrders);
+      _orderRepository.clearOrders();
     }
   }
 }
