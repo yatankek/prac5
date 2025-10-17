66631cd338e0249221bd66eb5a76c59198d648bb
yatankek <ninja_mos@mail.ru>
2025-10-17 23:27:51 +0300

    реализован репозиторий OrderRepository для хранения и управления заказами

---
+++ lib/features/orders/data/repository/order_repository.dart
@@ -0,0 +1,13 @@
+import 'package:prac5/features/orders/data/models/order.dart';
+
+class OrderRepository {
+  final List<Order> _orders = [];
+
+  List<Order> getAll() => List.unmodifiable(_orders);
+
+  void addOrder(Order order) => _orders.add(order);
+
+  void removeOrder(Order order) => _orders.remove(order);
+
+  double getTotalSum() => _orders.fold(0, (sum, o) => sum + o.total);
+}
